3f477e02445f3a03d1f8bb4c07db79d77ce22ef2
Lelequyngocquy <99171507+Lelequyngocquy@users.noreply.github.com>
2023-05-22 11:41:47 +0700

    Update CandyPlay.java (comment added)

--- CandyCraft_MineCrush/CandyPlay.java
+++ CandyCraft_MineCrush/CandyPlay.java
@@ -1,3 +1,13 @@
+/* Group 1. NH4, TTH2, Object Oriented Programming course, INTERNATIONAL UNIVERSITY - VIETNAM NATIONAL UNIVERSITY
+
+Le Ngoc Quy 		ITITIU21296
+Nguyen Thi Hai Yen 	ITITIU21353
+Nguyen Dy Nien 		ITITIU21272
+Nguyen Do Hoang Phi 	ITITIU21275
+ Purpose: containing the main function to execute the program
+*/
+
+
 public class CandyPlay{
 	public static void main(String[] args){
 		new Candy();
